234e86bce00f3c2ce6a0602eb248dd5d15885526
Max6613 <maxime66110@gmail.com>
2020-08-24 17:00:17 +0200

    sql

--- database/db.sql
+++ database/db.sql
@@ -213,4 +213,5 @@ INSERT INTO `settings` (`name`, `value`)
 VALUES ('#index-phrase', 'IL REVIENT ET IL EST PAS CONTENT ! MYTHONÉ EN PHP ET MYSQL.'),
        ('#truc-phrase', 'MAIS PUISQU''ON VOUS DIT QUE C''EST VRAI !'),
        ('#redac-phrase', 'LES REDACTEURS SONT LES MEILLEURS'),
-       ('#admin-phrase', 'L''ADMINISTRATION ...');
+       ('#admin-phrase', 'L''ADMINISTRATION ...'),
+       ('#admin-phrase', 'LES MENTIONS ILLEGALES C''EST PAS TRES LEGAL !');
